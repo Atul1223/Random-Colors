75c229a4e61767596c8bc95b7fe79db5ad75bcd8
Atul1223 <a1atulk@gmail.com>
2023-09-18 21:17:17 +0530

    Generated random colors and added it to table view
    Signed-off-by: Atul1223 <a1atulk@gmail.com>

--- RandomColors/ColorsTableVC.swift
+++ RandomColors/ColorsTableVC.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ColorsTableVC: UIViewController  {
     
+    var colors: [UIColor] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        addRandomColors()
+    }
+    
+    
+    func addRandomColors() {
+        for _ in 0..<50 {
+            colors.append(createRandomColor())
+        }
+    }
+    
+    func createRandomColor() -> UIColor {
+        let randomColor = UIColor(red: CGFloat.random(in: 0...1),
+                                  green: CGFloat.random(in: 0...1),
+                                  blue: CGFloat.random(in: 0...1),
+                                  alpha: 1)
+        return randomColor
     }
 }
 
@@ -22,12 +37,20 @@ extension ColorsTableVC: UITableViewDelegate, UITableViewDataSource {
     
     //how many number of rows needs to be showed
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 50
+        return colors.count
     }
     
     //what to shows in those rows
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        
+        //unwrap the optional using 'guard': In case 'ColorCell' identifier is not there
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ColorCell")
+        else {
+            return UITableViewCell()
+        }
+        let color = colors[indexPath.row]
+        cell.backgroundColor = color
+        return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
