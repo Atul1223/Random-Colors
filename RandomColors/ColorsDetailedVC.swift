e09b116876193fecb6b3ef40a77ef58ac47c8847
Atul1223 <a1atulk@gmail.com>
2023-09-18 18:53:43 +0530

    Navigation setup
    Signed-off-by: Atul1223 <a1atulk@gmail.com>

---
+++ RandomColors/ColorsDetailedVC.swift
@@ -0,0 +1,29 @@
+//
+//  ColorsDetailedVC.swift
+//  RandomColors
+//
+//  Created by Atul Kumar on 18/09/23.
+//
+
+import UIKit
+
+class ColorsDetailedVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
